10e1453b19a1ddfc236bfde6807fdae3e0f502aa
tyaga001 <tyaga001@live.com>
2022-03-18 18:26:58 +0530

    Added more code changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
-import './App.scss';
-import { useSessionContext } from 'supertokens-auth-react/recipe/session';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from '@apollo/client';
-import AppRoutes from './shared/components/routes/AppRoutes';
+import React from "react";
+import "./App.scss";
+import { useSessionContext } from "supertokens-auth-react/recipe/session";
+import { ApolloClient, InMemoryCache, ApolloProvider, DefaultOptions } from "@apollo/client";
+import AppRoutes from "./shared/components/routes/AppRoutes";
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+        fetchPolicy: "no-cache",
+        errorPolicy: "ignore",
+    },
+    query: {
+        fetchPolicy: "no-cache",
+        errorPolicy: "all",
+    },
+};
 
 function App() {
-  const { accessTokenPayload } = useSessionContext();
-  const client = new ApolloClient({
-    uri: `${process.env.REACT_APP_API_GRAPHQL_URL}`,
-    cache: new InMemoryCache(),
-    headers: {
-      Authorization: `Bearer ${accessTokenPayload?.jwt}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  return (
-    <div className="App">
-      <ApolloProvider client={client}>
-        <AppRoutes />
-      </ApolloProvider>
-    </div>
-  );
+    const { accessTokenPayload } = useSessionContext();
+    const client = new ApolloClient({
+        uri: `${process.env.REACT_APP_API_GRAPHQL_URL}`,
+        cache: new InMemoryCache(),
+        headers: {
+            Authorization: `Bearer ${accessTokenPayload?.jwt}`,
+            "Content-Type": "application/json",
+        },
+        defaultOptions,
+    });
+    return (
+        <div className="App">
+            <ApolloProvider client={client}>
+                <AppRoutes />
+            </ApolloProvider>
+        </div>
+    );
 }
 
 export default App;
